c3b1beebb0a8e05f5df8cf8836f978705ed01523
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-08-17 13:39:14 +0200

    refactor(project): cleanup WebUI HTML, JS and CSS code

--- project/srcMore/it/unibo/ctxRobot/QActorWebUI.js
+++ project/srcMore/it/unibo/ctxRobot/QActorWebUI.js
@@ -1,34 +1,35 @@
 //QActorWebUI.js
 var curSpeed = "low";
-	
-    console.log("QActorWebUI.js : server IP= "+document.location.host);
- /*
- * WEBSOCKET
- */
-    var sock = new WebSocket("ws://"+document.location.host, "protocolOne");
-    sock.onopen = function (event) {
-         //console.log("QActorWebUI.js : I am connected to server.....");
-         document.getElementById("connection").value = 'CONNECTED';
-    };
-    sock.onmessage = function (event) {
-        //console.log("QActorWebUI.js : "+event.data);
-        //alert( "onmessage " + event);
-        document.getElementById("state").value = ""+event.data;
-    }
-    sock.onerror = function (error) {
-        //console.log('WebSocket Error %0',  error);
-        document.getElementById("state").value = ""+error;
-    };
-    
-	function setSpeed(val){
-		curSpeed = val;
-		document.getElementById("speed").value = curSpeed;
-	}
-	function send(message) {
-		document.getElementById("sending").value = ""+message;
-		sock.send(message);
-	};
-/*	
+
+console.log("QActorWebUI.js : server IP= " + document.location.host);
+/*
+* WEBSOCKET
+*/
+var sock = new WebSocket("ws://" + document.location.host, "protocolOne");
+sock.onopen = function (event) {
+  //console.log("QActorWebUI.js : I am connected to server.....");
+  document.getElementById("connection").value = 'CONNECTED';
+};
+sock.onmessage = function (event) {
+  //console.log("QActorWebUI.js : "+event.data);
+  //alert( "onmessage " + event);
+  document.getElementById("state").value = "" + event.data;
+};
+sock.onerror = function (error) {
+  //console.log('WebSocket Error %0',  error);
+  document.getElementById("state").value = "" + error;
+};
+
+function setSpeed(val) {
+  curSpeed = val;
+  document.getElementById("speed").value = curSpeed;
+}
+
+function send(message) {
+  document.getElementById("sending").value = "" + message;
+  sock.send(message);
+}
+/*
 	document.onkeydown = function(event) {
 //	alert("event="+event);
 		    event = event || window.event;
@@ -50,4 +51,4 @@ var curSpeed = "low";
 		    }
 	};
 */
- 	//alert("loaded");
+//alert("loaded");
