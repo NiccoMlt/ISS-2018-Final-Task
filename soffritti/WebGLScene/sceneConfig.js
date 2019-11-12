d9d6bac0bfc2c06eefcf38d0c655edba3bb3f834
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-11-12 16:01:05 +0100

    feat(soffritti): improve virtual environment map config

--- soffritti/WebGLScene/sceneConfig.js
+++ soffritti/WebGLScene/sceneConfig.js
@@ -1,77 +1,46 @@
 const config = {
   floor: {
-      size: { x: 34, y: 30 }
+    size: { x: 20, y: 20 }
   },
   player: {
-      //position: { x: 0.5, y: 0.5 },		//CENTER
-      position: { x: 0.065, y: 0.075 },		//INIT
-      //position: { x: 0.8, y: 0.85 },		//END
-      speed: 0.038
+    // position: { x: 0.5, y: 0.5 },	// CENTER
+    position: { x: 0.12, y: 0.12 },		// INIT
+    // position: { x: 0.8, y: 0.85 }, // END
+    speed: 0.038
   },
-  sonars: [
-//        {
-//            name: "sonar1",
-//            position: { x: 0.064, y: 0.05 },
-//            senseAxis: { x: false, y: true }
-//        },
-//        {
-//            name: "sonar2",
-//            position: { x: 0.94, y: 0.88},
-//            senseAxis: { x: true, y: false }
-//        }
-   ],
-  movingObstacles: [
-     // {
-     //     name: "moving-obstacle-1",
-     //     position: { x: .5, y: .4 },
-     //     directionAxis: { x: true, y: false },
-     //     speed: 1,
-     //     range: 8
-     // }
-     // {
-     //     name: "moving-obstacle-2",
-     //     position: { x: .5, y: .2 },
-     //     directionAxis: { x: true, y: true },
-     //     speed: 2,
-     //     range: 2
-     // }
-  ],
+  sonars: [],
+  movingObstacles: [],
   staticObstacles: [
-//        {
-//            name: "wall",
-//            centerPosition: { x: 0.5, y: 0.9},
-//            size: { x: 0.01, y: 0.01}
-//        },
-      {
-          name: "wallUp",
-          centerPosition: { x: 0.5, y: 1},
-          size: { x: 1, y: 0.01}
-      },
-      {
-          name: "wallDown",
-          centerPosition: { x: 0.5, y: 0},
-          size: { x: 1, y: 0.01}
-      },
-      {
-          name: "wallLeft",
-          centerPosition: { x: 0, y: 0.5},
-          size: { x: 0.01, y: 1}
-      },
-      {
-          name: "wallRight",
-          centerPosition: { x: 1, y: 0.5},
-          size: { x: 0.01, y: 1}
-      },
-      {
-          name: "bag1",
-          centerPosition: { x: 0.4, y: 0.38 },
-          size: { x: 0.12, y: 0.045 }
-      },
-      {
-          name: "bag2",
-          centerPosition: { x: 0.75, y: 0.66 },
-          size: { x: 0.05, y: 0.045 }
-      },
+    {
+      name: "wallUp",
+      centerPosition: { x: 0.5, y: 1 },
+      size: { x: 1, y: 0.01 }
+    },
+    {
+      name: "wallDown",
+      centerPosition: { x: 0.5, y: 0 },
+      size: { x: 1, y: 0.01 }
+    },
+    {
+      name: "wallLeft",
+      centerPosition: { x: 0, y: 0.5 },
+      size: { x: 0.01, y: 1 }
+    },
+    {
+      name: "wallRight",
+      centerPosition: { x: 1, y: 0.5 },
+      size: { x: 0.01, y: 1 }
+    },
+    {
+      name: "bag1",
+      centerPosition: { x: 0.09, y: 0.42 },
+      size: { x: 0.1, y: 0.1 }
+    },
+    {
+      name: "bag2",
+      centerPosition: { x: 0.51, y: 0.96 },
+      size: { x: 0.1, y: 0.1 }
+    },
   ]
 }
 
